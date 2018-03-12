e606fb08cec84035a655b75f4a117ff496a1a52f
Huy Quang Nguyen <huyquang.nguyen@thomsonreuters.com>
2018-03-12 20:12:49 +1100

    init commit

---
+++ SimpleImageGallery/Models/GalleryIndexModel.cs
@@ -0,0 +1,11 @@
+﻿using SimpleImageGallery.Data.Models;
+using System.Collections.Generic;
+
+namespace SimpleImageGallery.Models
+{
+    public class GalleryIndexModel
+    {
+        public IEnumerable<GalleryImage> GalleryImages { get; set; }
+        public string SearchQuery { get; set; }
+    }
+}
